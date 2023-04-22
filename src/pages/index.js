cc42ca2055b4b5c78065a587c8059021c885196e
Vihan Raj <vihanraj@usc.edu>
2023-04-22 04:14:58 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,15 @@ export default function Home() {
   }
   return (
     <main className='h-screen w-screen bg-yellow flex flex-col'>
-        <input className='bg-white rounded-md border border-black text-black' type='text' placeholder='https://www.youtube.com/...'
+      <div className='my-auto px-24'>
+        <input className=' w-full bg-white rounded-md border border-black text-black' type='text' placeholder='https://www.youtube.com/...'
         value={url} onChange={(e)=>setUrl(e.target.value)}/>
-        <button className="block border px-4 py-2 w-1/2 mt-5 md:mt-0 md:w-auto mx-auto hover:bg-white hover:border-blue-dark hover:text-blue-dark text-sm rounded-full bg-blue-light text-white border-white transition-colors"
+        <button className="block border px-4 py-2 w-1/2 mt-5 md:mt-0 md:w-auto mx-auto hover:bg-white hover:border-black hover:text-black text-sm rounded-full bg-black text-white border-white transition-colors"
         onClick={()=>sendURL()}>
             Use Video
         </button>
+
+      </div>
     </main>
   );
 }
